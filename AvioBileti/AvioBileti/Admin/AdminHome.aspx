bbba214e462dbf62a99a28a73665c7675c976796
Simona Zeroska <zeroska.simona@gmail.com>
2014-06-15 23:35:57 +0200

    admin panel

--- AvioBileti/AvioBileti/Admin/AdminHome.aspx
+++ AvioBileti/AvioBileti/Admin/AdminHome.aspx
@@ -1,9 +1,184 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="AdminHome.aspx.cs" Inherits="AvioBileti.Admin.AdminHome" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+        .style4
+        {
+            width: 611px;
+        }
+        .style5
+        {
+            width: 132px;
+        }
+        .style7
+        {
+            width: 153px;
+        }
+    </style>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContentSpace" runat="server">
+    <table class="nav-justified">
+        <tr>
+            <td class="style4">
+        <asp:GridView ID="gwDestinacii" runat="server" CellPadding="4" ForeColor="#333333" 
+            GridLines="None" AutoGenerateColumns="False" DataKeyNames="idd" 
+                    onrowcancelingedit="gwDestinacii_RowCancelingEdit" 
+                    onrowediting="gwDestinacii_RowEditing" onrowupdating="gwDestinacii_RowUpdating" 
+                    onselectedindexchanged="gwDestinacii_SelectedIndexChanged">
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+            <Columns>
+                <asp:ButtonField CommandName="select" DataTextField="idd" HeaderText="ID" />
+                <asp:BoundField DataField="aerodrom" HeaderText="Аеродром" />
+                <asp:BoundField DataField="gradd" HeaderText="Град" />
+                <asp:CommandField CancelText="Откажи" DeleteText="Избриши" EditText="Уреди" 
+                    ShowEditButton="True" UpdateText="Уреди" />
+            </Columns>
+            <EditRowStyle BackColor="#999999" />
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <SortedAscendingCellStyle BackColor="#E9E7E2" />
+            <SortedAscendingHeaderStyle BackColor="#506C8C" />
+            <SortedDescendingCellStyle BackColor="#FFFDF8" />
+            <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:GridView>
+            </td>
+            <td>
+                <table class="style1">
+                    <tr>
+                        <td class="style7">
+                            ID</td>
+                        <td class="style5">
+                            <asp:TextBox ID="txtID" runat="server" Width="129px"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+                                ControlToValidate="txtID" ErrorMessage="Внесете ID!" ForeColor="Red">Внесете ID!</asp:RequiredFieldValidator>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Град</td>
+                        <td class="style5">
+                            <asp:TextBox ID="txtGrad" runat="server" Width="129px"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+                                ControlToValidate="txtGrad" ErrorMessage="Внесете град!" ForeColor="Red">Внесете град!</asp:RequiredFieldValidator>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Аеродром</td>
+                        <td class="style5">
+                            <asp:TextBox ID="txtAerodrom" runat="server" Width="129px"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" 
+                                ControlToValidate="txtAerodrom" ErrorMessage="Внесете аеродром!" 
+                                ForeColor="Red">Внесете аеродром!</asp:RequiredFieldValidator>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                        </td>
+                        <td class="style5">
+                            <asp:Button ID="btnDodadi" runat="server" onclick="btnDodadi_Click" 
+                                Text="Додади" />
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+        <tr>
+            <td class="style4">
+                &nbsp;</td>
+            <td>
+                &nbsp;</td>
+        </tr>
+        <tr>
+            <td class="style4">
+                <asp:GridView ID="gwLetovi" runat="server" AutoGenerateColumns="False" 
+                    DataKeyNames="idl" onrowcancelingedit="gwLetovi_RowCancelingEdit" 
+                    onrowediting="gwLetovi_RowEditing" onrowupdating="gwLetovi_RowUpdating" 
+                    onselectedindexchanged="gwLetovi_SelectedIndexChanged">
+                    <Columns>
+                        <asp:ButtonField CommandName="select" DataTextField="idl" HeaderText="IDL" 
+                            Text="Button" />
+                        <asp:BoundField DataField="datum" HeaderText="Датум" />
+                        <asp:BoundField DataField="vremep" HeaderText="Време на полетување" />
+                        <asp:BoundField DataField="vremes" HeaderText="Време на слетување" />
+                        <asp:BoundField DataField="vremetraenje" HeaderText="Времетраење" />
+                        <asp:BoundField DataField="tipAvion" HeaderText="Тип на авион" />
+                        <asp:BoundField DataField="cenafix" HeaderText="Цена" />
+                        <asp:CommandField CancelText="Откажи" DeleteText="Избриши" EditText="Уреди" 
+                            ShowEditButton="True" UpdateText="Уреди" />
+                    </Columns>
+                </asp:GridView>
+            </td>
+            <td>
+                <table class="nav-justified">
+                    <tr>
+                        <td class="style7">
+                            IDL</td>
+                        <td>
+                            <asp:TextBox ID="txtIDL" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Датум</td>
+                        <td>
+                            <asp:TextBox ID="txtDatum" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Време на полетување</td>
+                        <td>
+                            <asp:TextBox ID="txtVremeP" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Време на слетување</td>
+                        <td>
+                            <asp:TextBox ID="txtVremeS" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Времетраење</td>
+                        <td>
+                            <asp:TextBox ID="txtVremetraenje" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Тип на авион</td>
+                        <td>
+                            <asp:TextBox ID="txtTipAvion" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            Цена</td>
+                        <td>
+                            <asp:TextBox ID="txtCena" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="style7">
+                            &nbsp;</td>
+                        <td>
+                            <asp:Button ID="btnDodadiLet" runat="server" onclick="btnDodadiLet_Click" 
+                                Text="Додади" />
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
 </asp:Content>
 <asp:Content ID="Content3" ContentPlaceHolderID="FlightSearchSpace" runat="server">
+    <p>
+        <br />
+    </p>
 </asp:Content>
 <asp:Content ID="Content4" ContentPlaceHolderID="AdvertisingSpace" runat="server">
 </asp:Content>
--- AvioBileti/AvioBileti/Admin/AdminHome.aspx.cs
+++ AvioBileti/AvioBileti/Admin/AdminHome.aspx.cs
@@ -4,12 +4,283 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace AvioBileti.Admin
 {
     public partial class AdminHome : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ispolniDestinacii();
+                ispolniLetovi();
+            }
+
+        }
+
+        protected void ispolniDestinacii()
+        {
+            SqlConnection konekcija = new SqlConnection();
+            konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            string sqlStr = "SELECT * FROM destinacii ORDER BY gradd";
+            SqlCommand komanda = new SqlCommand(sqlStr, konekcija);
+            SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+            DataSet ds = new DataSet();
+
+            try
+            {
+                konekcija.Open();
+                adapter.Fill(ds, "destinacii");
+                gwDestinacii.DataSource = ds;
+                gwDestinacii.DataBind();
+
+                ViewState["dataset"] = ds;
+            }
+            catch (Exception err)
+            {
+                //lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+        }
+
+        protected void gwDestinacii_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gwDestinacii_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            DataSet ds = (DataSet)ViewState["dataset"];
+            gwDestinacii.EditIndex = -1;
+            gwDestinacii.DataSource = ds;
+            gwDestinacii.DataBind();
+        }
+
+        protected void gwDestinacii_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            DataSet ds = (DataSet)ViewState["dataset"];
+            gwDestinacii.EditIndex = e.NewEditIndex;
+            gwDestinacii.DataSource = ds;
+            gwDestinacii.DataBind();
+        }
+
+        protected void gwDestinacii_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            SqlConnection konekcija = new SqlConnection();
+            konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            string sqlUpdate = "UPDATE destinacii set gradd = @gradd, aerodrom = @aerodrom WHERE idd = @idd";
+            SqlCommand updateKomanda = new SqlCommand(sqlUpdate, konekcija);
+            TextBox tb = (TextBox)gwDestinacii.Rows[e.RowIndex].Cells[1].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@gradd", tb.Text);
+
+            tb = (TextBox)gwDestinacii.Rows[e.RowIndex].Cells[2].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@aerodrom", tb.Text);
+
+            updateKomanda.Parameters.AddWithValue("@idd", gwDestinacii.DataKeys[gwDestinacii.SelectedIndex].Value);
+                //Rows[e.RowIndex].Cells[0].Text);
+
+            int efekt = 0;
+            try
+            {
+                konekcija.Open();
+                efekt = updateKomanda.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                //lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                konekcija.Close();
+                gwDestinacii.EditIndex = -1;
+            }
+
+            if (efekt != 0)
+                ispolniDestinacii();
+        }
+
+        protected void btnDodadi_Click(object sender, EventArgs e)
+        {
+            SqlConnection konekcija = new SqlConnection();
+            konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            string sqldodadi = "INSERT INTO destinacii (idd, gradd, aerodrom) VALUES (@idd, @gradd, @aerodrom)";
+            SqlCommand dodadiKomanda = new SqlCommand(sqldodadi, konekcija);
+
+            dodadiKomanda.Parameters.AddWithValue("@idd", txtID.Text);
+            dodadiKomanda.Parameters.AddWithValue("@gradd", txtGrad.Text);
+            dodadiKomanda.Parameters.AddWithValue("@aerodrom", txtAerodrom.Text);
+
+            int efekt = 0;
+            try
+            {
+                konekcija.Open();
+                efekt = dodadiKomanda.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                //lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+            if (efekt != 0)
+            {
+                ispolniDestinacii();
+                txtID.Text = "";
+                txtGrad.Text = "";
+                txtAerodrom.Text = "";
+            }
+        }
+
+        protected void ispolniLetovi()
+        {
+            SqlConnection konekcija = new SqlConnection();
+            konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            string sqlStr = "SELECT * FROM letovi ORDER BY idl";
+            SqlCommand komanda = new SqlCommand(sqlStr, konekcija);
+            SqlDataAdapter adapter = new SqlDataAdapter(komanda);
+            DataSet ds = new DataSet();
+
+            try
+            {
+                konekcija.Open();
+                adapter.Fill(ds, "letovi");
+                gwLetovi.DataSource = ds;
+                gwLetovi.DataBind();
+
+                ViewState["datasetLet"] = ds;
+            }
+            catch (Exception err)
+            {
+                //lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+        }
+
+
+
+        protected void btnDodadiLet_Click(object sender, EventArgs e)
+        {
+            SqlConnection konekcija = new SqlConnection();
+            konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            string sqldodadi = "INSERT INTO letovi (idl, datum, vremep, vremes, vremetraenje, tipavion, cenafix) VALUES (@idl, @datum, @vremep, @vremes, @vremetraenje, @tipavion, @cenafix)";
+            SqlCommand dodadiKomanda = new SqlCommand(sqldodadi, konekcija);
+
+            dodadiKomanda.Parameters.AddWithValue("@idl", txtID.Text);
+            dodadiKomanda.Parameters.AddWithValue("@datum", txtDatum.Text);
+            dodadiKomanda.Parameters.AddWithValue("@vremep", txtVremeP.Text);
+            dodadiKomanda.Parameters.AddWithValue("@vremes", txtVremeS.Text);
+            dodadiKomanda.Parameters.AddWithValue("@vremetraenje", txtVremetraenje.Text);
+            dodadiKomanda.Parameters.AddWithValue("@tipavion", txtTipAvion.Text);
+            dodadiKomanda.Parameters.AddWithValue("@cenafix", txtCena.Text);
+
+            int efekt = 0;
+            try
+            {
+                konekcija.Open();
+                efekt = dodadiKomanda.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                //lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+            if (efekt != 0)
+            {
+                ispolniLetovi();
+                txtID.Text = "";
+                txtDatum.Text = "";
+                txtVremeP.Text = "";
+                txtVremeS.Text = "";
+                txtVremetraenje.Text = "";
+                txtTipAvion.Text = "";
+                txtCena.Text = "";              
+            }
+
+        }
+
+        protected void gwLetovi_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            DataSet ds = (DataSet)ViewState["datasetLet"];
+            gwLetovi.EditIndex = -1;
+            gwLetovi.DataSource = ds;
+            gwLetovi.DataBind();
+
+        }
+
+        protected void gwLetovi_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            DataSet ds = (DataSet)ViewState["datasetLet"];
+            gwLetovi.EditIndex = e.NewEditIndex;
+            gwLetovi.DataSource = ds;
+            gwLetovi.DataBind();
+
+        }
+
+        protected void gwLetovi_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            SqlConnection konekcija = new SqlConnection();
+            konekcija.ConnectionString = ConfigurationManager.ConnectionStrings["ApplicationServices"].ConnectionString;
+            string sqlUpdate = "UPDATE letovi set datum = @datum, vremep = @vremep, vremes = @vremes, vremetraenje = @vremetraenje, tipavion = @tipavion, cenafix = @cenafix WHERE idl = @idl";
+
+            
+            SqlCommand updateKomanda = new SqlCommand(sqlUpdate, konekcija);
+            TextBox tb = (TextBox)gwLetovi.Rows[e.RowIndex].Cells[1].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@datum", tb.Text);
+
+            tb = (TextBox)gwLetovi.Rows[e.RowIndex].Cells[2].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@vremep", tb.Text);
+
+            tb = (TextBox)gwLetovi.Rows[e.RowIndex].Cells[2].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@vremes", tb.Text);
+
+            tb = (TextBox)gwLetovi.Rows[e.RowIndex].Cells[2].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@vremetraenje", tb.Text);
+
+            tb = (TextBox)gwLetovi.Rows[e.RowIndex].Cells[2].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@tipavion", tb.Text);
+
+            tb = (TextBox)gwLetovi.Rows[e.RowIndex].Cells[2].Controls[0];
+            updateKomanda.Parameters.AddWithValue("@cenafix", tb.Text);
+
+            updateKomanda.Parameters.AddWithValue("@idl", gwLetovi.DataKeys[gwLetovi.SelectedIndex].Value);
+            //Rows[e.RowIndex].Cells[0].Text);
+
+            int efekt = 0;
+            try
+            {
+                konekcija.Open();
+                efekt = updateKomanda.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                //lblPoraka.Text = err.Message;
+            }
+            finally
+            {
+                konekcija.Close();
+                gwLetovi.EditIndex = -1;
+            }
+
+            if (efekt != 0)
+                ispolniLetovi();
+        }
+
+        protected void gwLetovi_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
